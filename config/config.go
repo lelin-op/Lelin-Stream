a9b01684a24193713401a07f0af5f822f16e2a2a
Lp LÉLÍÑ <114496734+lp-lelin@users.noreply.github.com>
2026-05-03 00:50:57 +0600

    Delete config/config.go

--- config/config.go
+++
@@ -1,9 +0,0 @@
-// Updated loadFromEnvFile function to make fsb.env optional.
-
-func loadFromEnvFile(path string) {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		log.Println("INFO: Env file does not exist, falling back to environment variables.")
-		return
-	}
-	// Existing loading logic...
-}
